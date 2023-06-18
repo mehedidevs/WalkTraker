25961165997e393be6a9688be12d1ee186261b11
Md. Mehedi Hasan <108575130+mehedidevs@users.noreply.github.com>
2023-06-18 19:43:30 +0600

    initial commit

---
+++ app/src/main/java/com/mehedi/walktraker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mehedi.walktraker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
